583ab956cb8afbcce7192289267f41e70ea1be6c
Samuel Romero <sromerom@esliceu.net>
2019-10-10 17:56:58 +0200

    Millorant petites coses i implementant favicon

--- llibre.php
+++ llibre.php
@@ -1,20 +1,47 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="ca">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="En aquesta pàgina trobaràs tota classe de llibres a on podràs des de visualitzar les dades d'un llibre
+    fins i tot comprar-los sense cap tipus de problema. Disponible tant en físic com en electrònic.">
+    <meta name="keywords" content="Llibres,Llibreria,Online,Electronic,Paper,Projecte,Liceu">
+    <meta name="author" content="Samuel Romero Marín">
+
+    <!-- CSS -->
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/llibre.css">
 
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="src/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="src/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="src/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="src/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="src/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="src/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="src/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="src/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="src/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="src/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="src/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="src/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="src/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- fontawesome -->
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <title>Compra Llibres</title>
+    <title>Compra Llibres | Dades del Llibre</title>
 </head>
 
 <body>
@@ -22,9 +49,9 @@
         <section id="menu">
             <h1><img src="../src/logo.png" alt="Logo de la pàgina" id="logo"></h1>
             <ul>
-                <li><a href="principal.php" class="btn btn-info btn-lg">Inici</a></li>
-                <li><a href="formulari.php" class="btn btn-info btn-lg">Gestiona</a></li>
-                <li><a href="endLogin.php" class="btn btn-info btn-lg">Surt</a></li>
+                <li><a href="principal.php" class="btn btn-light btn-lg">Inici</a></li>
+                <li><a href="formulari.php" class="btn btn-light btn-lg">Gestiona</a></li>
+                <li><a href="endLogin.php" class="btn btn-light btn-lg">Surt</a></li>
             </ul>
             <p>Benvingut
                 <?php
@@ -35,7 +62,7 @@
                 ?>
             </p>
         </section>
-    </header>F
+    </header>
     <main>
         <h2>Dades del llibre:</h2>
         <?php
@@ -48,7 +75,7 @@
 
 
             if ($resCon) {
-                
+
                 $fila = $resCon->fetch_assoc();
                 echo '<img src="' . $fila['uri'] . '"><br>';
 
