f0f33edef3ebd23acda3ebe0e5548754414d5a57
Samuel Romero <sromerom@esliceu.net>
2019-11-15 11:45:35 +0100

    Afegit script de Google Analytics

--- formulari.php
+++ formulari.php
@@ -58,6 +58,19 @@ if ($varSession == null || $varSession = '') {
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- Seguiment amb google analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152549941-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-152549941-1');
+    </script>
     <title>Compra Llibres | Gestió dels llibres</title>
 </head>
 
@@ -138,7 +151,7 @@ if ($varSession == null || $varSession = '') {
                         <input id="txt_Insertaiddelproducteaeliminar" name="id_llibre" type="text" aria-labelledby="Insertaiddelproducteaeliminar-ariaLabel" class="required" title="Inserta id del producte a eliminar:. Aquest es un camp obligatori">
                     </div>
                     <div class="row">
-                    <button type="submit" name="formulari2" class="btn btn-danger">Elimina</button>
+                        <button type="submit" name="formulari2" class="btn btn-danger">Elimina</button>
                     </div>
                 </form>
 
--- login.php
+++ login.php
@@ -41,12 +41,28 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- Seguiment amb google analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152549941-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-152549941-1');
+    </script>
+
+
     <title>Compra Llibres | Tenda virtual de llibres i Ebooks</title>
 </head>
 
 <body>
     <main class="inicisessio">
-        <img src="src/fotoLogin.png" class="avatar"alt="Logo login">
+        <img src="src/fotoLogin.png" class="avatar" alt="Logo login">
         <h2>Inicia sessió:</h2>
         <form action="index.php" method="POST">
             <?php
